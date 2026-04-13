652e5c5ab86eed99b8c3922b57021d4cd8278612
Norfolk <alexander.garofalo.ag@gmail.com>
2026-04-12 20:23:50 -0500

    P3.V4- Mover personaje izq-der js

--- limones.js
+++ limones.js
@@ -4,6 +4,7 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+let personajeX=canvas.width/2;
 
 function iniciar(){
     dibujarSuelo();
@@ -17,5 +18,26 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle="#ffe600";
-    ctx.fillRect(canvas.width/2 - ANCHO_PERSONAJE/2, canvas.height - (ALTURA_SUELO + ALTURA_PERSONAJE), ANCHO_PERSONAJE, ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX, canvas.height - (ALTURA_SUELO + ALTURA_PERSONAJE), ANCHO_PERSONAJE, ALTURA_PERSONAJE);
+}
+
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla();
+}
+
+function moverDerecha(){
+    personajeX=personajeX+10;
+    actualizarPantalla();
+}
+
+function actualizarPantalla(){
+    limpiarCanvas();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function limpiarCanvas(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    dibujarSuelo();
 }
